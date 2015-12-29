c528bfbd89b7405c0d622a317025c6cf3031ce85
smghacker <valeri.colov@gmail.com>
2015-12-29 09:47:40 +0200

    Adding Gray Level Histogram class.

--- SimpleImageProcessor/SimpleImageProcessor.pro
+++ SimpleImageProcessor/SimpleImageProcessor.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    graylevelhistogram.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    graylevelhistogram.h
 
 FORMS    += mainwindow.ui
---
+++ SimpleImageProcessor/graylevelhistogram.cpp
@@ -0,0 +1,18 @@
+#include "graylevelhistogram.h"
+
+QVector<int> GrayLevelHistogram::getHistogram(QImage *image)
+{
+    QVector<int> histogram = QVector<int>(256);
+    QRgb valueOfPixel;
+    QRgb grayValue;
+    for(int y = 0; y < image->height(); y++)
+    {
+        for(int x = 0; x < image->width(); x++)
+        {
+            valueOfPixel = image->pixel(x, y);
+            grayValue = qGray(valueOfPixel);
+            histogram[grayValue]++;
+        }
+    }
+    return histogram;
+}
