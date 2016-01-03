08fe32d00b5f23342832ee1c53ffefea77d5dadd
son4etyyy <son4etyyy@gmail.com>
2016-01-03 22:04:07 +0200

    remove not needed comments

--- SimpleImageProcessor/grayscale.cpp
+++ SimpleImageProcessor/grayscale.cpp
@@ -1,31 +1,20 @@
 #include "grayscale.h"
 
 
-void GrayScale::apply(QImage &srcImage){
-    /*for (int ii = 0; ii < srcImage.height(); ii++) {
-        uchar* scan = srcImage.scanLine(ii);
-        int depth =4;
-        for (int jj = 0; jj < srcImage.width(); jj++) {
+void GrayScale::apply(QImage &srcImage)
+{
+    QImage dstImage = srcImage.convertToFormat(srcImage.hasAlphaChannel() ?
+              QImage::Format_ARGB32 : QImage::Format_RGB32);
 
-            QRgb* rgbpixel = reinterpret_cast<QRgb*>(scan + jj*depth);
-            int gray = qGray(*rgbpixel);
-            *rgbpixel = QColor(gray, gray, gray).rgba();
-        }
-    }*/
-    // Convert to 32bit pixel format
-         QImage dstImage = srcImage.convertToFormat(srcImage.hasAlphaChannel() ?
-                  QImage::Format_ARGB32 : QImage::Format_RGB32);
+     unsigned int *data = (unsigned int*)dstImage.bits();
+     int pixelCount = dstImage.width() * dstImage.height();
 
-         unsigned int *data = (unsigned int*)dstImage.bits();
-         int pixelCount = dstImage.width() * dstImage.height();
+     for(int i = 0; i < pixelCount; ++i) {
+        int val = qGray(*data);
+        *data = qRgba(val, val, val, qAlpha(*data));
+        ++data;
+     }
 
-         // Convert each pixel to grayscale
-         for(int i = 0; i < pixelCount; ++i) {
-            int val = qGray(*data);
-            *data = qRgba(val, val, val, qAlpha(*data));
-            ++data;
-         }
-
-         srcImage = QImage(dstImage);
+     srcImage = QImage(dstImage);
 }
 
