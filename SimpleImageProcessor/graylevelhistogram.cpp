3a4745ac120f130f2d2fc1be310765b2a8a7e5b4
smghacker <valeri.colov@gmail.com>
2016-02-08 21:14:07 +0200

    Adding documentation to code. Scaling Y-axis of histogram to have the length of the maximum number of pixels of one shade of gray

--- SimpleImageProcessor/graylevelhistogram.cpp
+++ SimpleImageProcessor/graylevelhistogram.cpp
@@ -1,4 +1,5 @@
 #include "graylevelhistogram.h"
+
 QVector<double> GrayLevelHistogram::getHistogram(QImage image)
 {
     QVector<double> histogram = QVector<double>(256);
@@ -17,16 +18,12 @@ QVector<double> GrayLevelHistogram::getHistogram(QImage image)
 }
 
 void GrayLevelHistogram::drawHistogram(QCustomPlot *customPlot, QImage image){
-    //trea se za4isti
-    // create empty bar chart objects:
     QCPBars *pixelsBars = new QCPBars(customPlot->xAxis, customPlot->yAxis);
     customPlot->addPlottable(pixelsBars);
-    //customPlot->setWindowFlags(Qt::Popup);
     pixelsBars->setName("Gray Level Histogram");
     pixelsBars->setBrush(QColor(150, 222, 0, 70));
     pixelsBars->setWidth(1);
 
-    // prepare x axis with country labels:
     customPlot->xAxis->setAutoTicks(true);
     customPlot->xAxis->setAutoTickLabels(true);
     customPlot->xAxis->setTickLabelRotation(60);
@@ -35,8 +32,6 @@ void GrayLevelHistogram::drawHistogram(QCustomPlot *customPlot, QImage image){
     customPlot->xAxis->grid()->setVisible(true);
     customPlot->xAxis->setRange(0, 256);
 
-    // prepare y axis:
-    customPlot->yAxis->setRange(0, 600);
     customPlot->yAxis->setLabel("Number of pixels");
     customPlot->yAxis->grid()->setSubGridVisible(true);
     QPen gridPen;
@@ -46,9 +41,16 @@ void GrayLevelHistogram::drawHistogram(QCustomPlot *customPlot, QImage image){
     gridPen.setStyle(Qt::DotLine);
     customPlot->yAxis->grid()->setSubGridPen(gridPen);
 
-    // Add data:
     QVector<double> regenData = this->getHistogram(image);
     QVector<double> ticks = QVector<double>(256);
+    double maxValue = -1;
+    for(int i = 0; i < 256; i++){
+        if(maxValue < regenData[i]){
+            maxValue = regenData[i];
+        }
+    }
+    //scaling the y-axis, so to cover the max value
+    customPlot->yAxis->setRange(0, maxValue);
     for(int i = 0; i < 256; i++){
         ticks[i] = i;
     }
