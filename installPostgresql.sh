b68ee2564b66ff6c01b308d7898628fa0f8c1377
smghacker <valeri.colov@gmail.com>
2016-06-19 11:18:54 +0300

    Polishing the script. Automating the replacement of the configuration for domain socket. Add user. Remove installation of the pgAdmin3. Parameterizing the password of the user.

--- installPostgresql.sh
+++ installPostgresql.sh
@@ -1,20 +1,14 @@
-sudo apt-get update
-sudo apt-get install postgresql-9.3
+mypassword=$1
+echo $mypassword | sudo -S apt-get -y update
+echo $mypassword | sudo -S apt-get -y install postgresql-9.3
 export PATH=/usr/lib/postgresql/9.3/bin:$PATH:/usr/sbin
 export PGDATA=/var/lib/postgresql/9.3/main
 export PGPORT=5432
-sudo apt-get install postgresql postgresql-contrib
-sudo apt-get install pgadmin3
-sudo -u postgres psql postgres
-sudo -u postgres createdb mydb
-sudo apt-get install postgresql-contrib
-sudo nano /etc/postgresql/9.3/main/pg_hba.conf
-# instead:
-# # Database administrative login by Unix domain socket
-# local   all             postgres                                peer
-# write:
-# # Database administrative login by Unix domain socket
-# local   all             postgres                                md5
-
-sudo /etc/init.d/postgresql reload
-pgadmin3
+echo $mypassword | sudo -S apt-get -y install postgresql postgresql-contrib
+echo $mypassword | sudo -u -S postgres psql postgres
+echo $mypassword | sudo -u -S postgres createdb mydb
+echo $mypassword | sudo -S apt-get -y install postgresql-contrib
+echo $mypassword | sudo sed -e s/'local   all             postgres                                peer'/'local   all             postgres                                md5'/ /etc/postgresql/9.3/main/pg_hba.conf
+echo $mypassword | sudo -S /etc/init.d/postgresql reload
+userToAdd=whoami
+echo $mypassword | sudo -u postgres createuser $userToAdd
